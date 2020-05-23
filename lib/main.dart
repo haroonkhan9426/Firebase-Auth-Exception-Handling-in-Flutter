e5e4020e7c85e24380f660cbefd3228f371e2af8
Haroon khan <haroonkhan9426@gmail.com>
2020-05-23 15:34:52 +0500

    Directory structure done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:base_flutter_project/ui/pages/splash-screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,6 +7,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: SplashScreen(),
+    );
   }
 }
